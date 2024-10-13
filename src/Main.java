1da24ffb0ae4c3abd660a0cb23ff201a6d76dc68
XomaH <kympol40@gmail.com>
2024-10-13 17:28:39 +0300

    inition commit

--- src/Main.java
+++ src/Main.java
@@ -16,18 +16,34 @@ public class Main {
 
         int clientDeviceYear =2015;
         {
-            if (clientDeviceYear >=  2015 && clientOS == 0)
-            {
+            if (clientDeviceYear >= 2015 && clientOS == 0) {
                 System.out.println("Установите версию приложения для iOS по ссылке");
-            }
-            else
+            } else if (clientDeviceYear < 2015 && clientOS == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else if (clientOS == 1 && clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else
             {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                System.out.println("Установите версию приложения для Android по ссылке");
             }
         }
 
 
-
+    int year =2021;
+        {
+            if (year < 1584)
+            {
+                System.out.println("До 1584 не использовался високосный год");
+            }
+            else if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) )
+            {
+                System.out.println(year + " год является високосным");
+            }
+            else
+            {
+                System.out.println(year + " год не является високосным");
+            }
+        }
 
 
     }
