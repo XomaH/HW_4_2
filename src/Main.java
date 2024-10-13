7777db8169c46c493d8aa8f6eef00d0ad917d7eb
XomaH <kympol40@gmail.com>
2024-10-13 18:19:41 +0300

    inition commit

--- src/Main.java
+++ src/Main.java
@@ -45,6 +45,61 @@ public class Main {
             }
         }
 
+        int deliveryDistance =95;
+        int deliveryDay =0;
+
+        if (deliveryDistance <= 20)
+        {
+            deliveryDay += 1;
+            System.out.println("Потребуется дней: 1");
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60)
+        {
+            deliveryDay += 1;
+            System.out.println("Потребуется дней: 2");
+        }
+        else if (deliveryDistance > 60 && deliveryDistance <= 100)
+        {
+            deliveryDay += 1;
+            System.out.println("Потребуется дней: 3");
+        }
+        else {
+            System.out.println("Свыше 100 км доставки нет");
+        }
+
+
+
+        int monthNumber =12;
+        switch (monthNumber)  {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");;
+                break;
+            case 3:
+            case 4:
+            case 5:
+
+                    System.out.println("Весна");
+                    break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+
+            default:
+                System.out.println("В году 12 месяцев");
 
     }
+
+
+
+
+
+}
 }
