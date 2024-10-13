e2b83133526acb11ec3c2e1424be8a89a86a05ec
XomaH <kympol40@gmail.com>
2024-10-13 16:59:29 +0300

    inition commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+
+     int clientOS = 0;
+        {
+            if (clientOS == 0);
+            {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+            else
+            {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
+
+    int clientDeviceYear =2015;
+        {
+            if (clientDeviceYear > 2015 && clientOS == 0);
+            {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+            else
+            {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        }
+
+
+
+
+
     }
 }
